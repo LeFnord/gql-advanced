52712f0d6d317e055a692ab04c5ecf392efe46e4
LeFnord <pscholz.le@gmail.com>
2022-10-18 17:58:53 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vite + Vue3 + VueRouter
+
+
+This starter template also includes:
+
+- [Vue Router 4.x](https://github.com/vuejs/vue-router-next)
+
+
+### To Do
+
+- clone it,
+- install dependencies: `yarn install`
+- run it: `yarn dev`
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header v-if="$route.meta.title">
+      <h1 @click="counter = 0" >
+        {{ $route.meta.title }} / {{ counter }}
+      </h1>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let counter = ref(0)
+
+setInterval(() => {
+  counter.value++
+}, 1000)
+</script>
+
+<style>
+body {
+  font-family: 'Inter Roman', 'Helvetica Neue', Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
